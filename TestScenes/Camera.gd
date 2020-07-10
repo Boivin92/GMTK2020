b921dd9603f82578ee6e86665f36de3ba0cdafca
Francois Boivin <boivin.92@gmail.com>
2020-07-10 18:32:14 -0400

    test bateau qui tangue

---
+++ TestScenes/Camera.gd
@@ -0,0 +1,17 @@
+extends Camera
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	set_as_toplevel(true)
+	pass # Replace with function body.
+
+
+func _physics_process(delta):
+#	self.rotation.z = -(get_parent().rotation.z)
+	pass
