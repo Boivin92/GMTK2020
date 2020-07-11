7e5e29c3739a7d7acb9eb8277030615a03350980
Francois Boivin <boivin.92@gmail.com>
2020-07-11 16:38:09 -0400

    fix pour le score

--- Cargo/Crate.gd
+++ Cargo/Crate.gd
@@ -3,12 +3,15 @@ extends RigidBody
 class_name Crate
 
 export (int) var cargo_value = 50
+var lost = false
 
 func _ready():
 	Score.register_new_cargo(self)
 
 func is_lost():
-	Score.cargo_lost(self)
+	if not lost:
+		Score.cargo_lost(self)
+		lost = true
 	if not $SplouchePlayer.playing:
 		$SplouchePlayer.play_random()
 
@@ -18,7 +21,7 @@ func _on_SplouchePlayer_finished():
 
 
 func _on_Box_body_entered(body):
-	if body.is_in_group("crate"):
+	if body.is_in_group("crate") && not lost:
 		if self.linear_velocity.length() > 0.5:
 			if body.is_in_group("wood") && self.is_in_group("wood"):
 				$CollisionPlayer.play_random([self,body], "WoodOnWood")
@@ -26,7 +29,6 @@ func _on_Box_body_entered(body):
 				$CollisionPlayer.play_random([self,body], "MetalOnMetal")
 			else:
 				$CollisionPlayer.play_random([self,body], "MetalOnWood")
-	pass # Replace with function body.
 
 
 func _on_FloatTimer_timeout():
