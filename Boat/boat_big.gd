e2476e038bd03db044a02057106a098f37f2bc6d
Francois Boivin <boivin.92@gmail.com>
2020-07-11 13:56:09 -0400

    import de plein d'assets

--- Boat/boat_big.gd
+++ Boat/boat_big.gd
@@ -5,4 +5,7 @@ var time_itself = 0
 func _physics_process(delta):
 	time_itself+=delta
 	self.rotation.z = sin(time_itself/5)/5
-	self.rotation.x = cos(time_itself/5)/5
+	self.rotation.x = -sin(time_itself/7)/8
+	if time_itself < 1:
+		print("x: " + str(self.rotation_degrees.x))
+		print("z: " + str(self.rotation_degrees.z))
