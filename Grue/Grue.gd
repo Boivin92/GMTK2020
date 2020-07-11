592b5f93293a187dadfd58045d60a7c514803f22
xenoexplorator <cedric.pinard@gmail.com>
2020-07-10 21:29:10 -0400

    Ajout de mobilité à la grue

--- Grue/Grue.gd
+++ Grue/Grue.gd
@@ -1,12 +1,14 @@
 extends Spatial
 
+export(float) var vitesse := 10.0
+
 enum State {
 	Raised,
 	Raising,
 	Lowered,
 	Lowering,
 }
-export (State) var state = State.Raised
+var state = State.Raised
 
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("activate"):
@@ -15,3 +17,19 @@ func _process(delta: float) -> void:
 				$AnimationPlayer.play("Activate")
 			State.Lowered:
 				$AnimationPlayer.play("Deactivate")
+	
+	if state == State.Raised:
+		var move = Vector3()
+		if Input.is_action_pressed("ui_left"):
+			move.x -= vitesse
+		if Input.is_action_pressed("ui_right"):
+			move.x += vitesse
+		if Input.is_action_pressed("ui_up"):
+			move.z -= vitesse
+		if Input.is_action_pressed("ui_down"):
+			move.z += vitesse
+		if move != Vector3.ZERO:
+			translate(move * delta)
+
+func set_state(s):
+	state = s
