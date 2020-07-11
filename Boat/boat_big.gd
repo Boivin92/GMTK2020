7263a38154ab3c86d07cb7fd10bdc847cb27e2cd
Francois Boivin <boivin.92@gmail.com>
2020-07-11 15:03:46 -0400

    exposition des variables de branlement du bateau

--- Boat/boat_big.gd
+++ Boat/boat_big.gd
@@ -1,11 +1,13 @@
 extends StaticBody
 
 var time_itself = 0
+export (float) var z_axis_dampening = 5
+export (float) var z_axis_speed = 5
+export (float) var x_axis_dampening = 7
+export (float) var x_axis_speed = 8
+
 
 func _physics_process(delta):
 	time_itself+=delta
-	self.rotation.z = sin(time_itself/5)/5
-	self.rotation.x = -sin(time_itself/7)/8
-	if time_itself < 1:
-		print("x: " + str(self.rotation_degrees.x))
-		print("z: " + str(self.rotation_degrees.z))
+	self.rotation.z = sin(time_itself/z_axis_dampening)/z_axis_speed
+	self.rotation.x = -sin(time_itself/x_axis_dampening)/x_axis_speed
